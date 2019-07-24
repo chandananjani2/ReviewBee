ff69a8b58babecda9a58a34d7a22e1d17163025c
chandananjani2 <53098530+chandananjani2@users.noreply.github.com>
2019-07-24 12:46:45 -0700

    Update Startup.cs

--- Application/aspnet-core-dotnet-core/Startup.cs
+++ Application/aspnet-core-dotnet-core/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+//test
 namespace aspnet_core_dotnet_core
 {
     public class Startup
